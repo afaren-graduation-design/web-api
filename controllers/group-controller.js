243b46a1b5d55cb008feb5a91eabeb021cd3fe76
Evan-Leee <EvanLee_@outlook.com>
2016-04-12 15:15:37 +0800

    add the group-controller

---
+++ controllers/group-controller.js
@@ -0,0 +1,13 @@
+'use strict';
+var apiRequest = require('../services/api-request');
+
+function GroupController (){
+
+}
+
+GroupController.prototype.getGroupInfo = (req, res) => {
+  var
+  apiRequest.get('groups',)
+};
+
+module.exports = GroupController;
--- routes/routers/group.js
+++ routes/routers/group.js
@@ -3,9 +3,12 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var GroupController = require('../../controllers/group-controller');
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname, '../../public/assets', 'group.html'));
 });
+
+router.get('/index',GroupController.getGroupInfo);
 
 module.exports = router;
