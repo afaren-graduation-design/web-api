4dbc8c78349f42f01599b1bb154ff3f3f68e48b3
Evan-Leee <EvanLee_@outlook.com>
2016-04-12 16:20:53 +0800

    repair the group bug

--- controllers/group-controller.js
+++ controllers/group-controller.js
@@ -1,13 +1,15 @@
 'use strict';
 var apiRequest = require('../services/api-request');
+var async = require('async');
 
 function GroupController (){
 
 }
 
 GroupController.prototype.getGroupInfo = (req, res) => {
-  var
-  apiRequest.get('groups',)
+  //var userId = req.session.user.id;
+  var groupId = req.query.groupId;
+  //apiRequest.get('groups',{id:groupId})
 };
 
 module.exports = GroupController;
--- routes/routers/group.js
+++ routes/routers/group.js
@@ -4,11 +4,12 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var GroupController = require('../../controllers/group-controller');
+var groupController = new GroupController();
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname, '../../public/assets', 'group.html'));
 });
 
-router.get('/index',GroupController.getGroupInfo);
+router.get('/index',groupController.getGroupInfo);
 
 module.exports = router;
