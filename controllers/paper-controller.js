178fe0801e7204d10bc06dcf263f81ca5f17ccf9
purpletianjing <TianjingSun@outlook.com>
2016-04-19 14:29:04 +0800

    delete many consoles

--- controllers/paper-controller.js
+++ controllers/paper-controller.js
@@ -9,25 +9,18 @@ function PaperController (){
 
 PaperController.prototype.operatePaper = (req, res, next) => {
 
-  console.log('1111111111')
   var paperInfo = {
     paperName: req.body.paperName,
     makerId: req.session.user.id
   };
-  console.log("controller controller");
-
 
   apiRequest.post('papers', paperInfo, function(err, resp) {
-    console.log(paperInfo);
-    console.log("=============");
-
 
     if (resp === undefined) {
       res.send({
         status: constant.httpCode.INTERNAL_SERVER_ERROR
       });
     } else if(resp.status === constant.httpCode.OK) {
-      console.log(resp.body);
       res.send({
         status: constant.httpCode.CREATED,
         paperId: resp.body.paperId
