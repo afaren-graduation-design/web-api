3fd8d0eaee901768fc878a3a7f91e743d9e3e098
sialvsic <sialvsic@outlook.com>
2016-04-20 20:31:49 +0800

    add captcha in express app use

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ var route = require('./routes/route');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var sessionCheck = require('./middleware/session-check');
-var errRequestHandler = require('./middleware/errorRequestHandler');
 var util = require('util');
 var mongoConn = require('./services/mongo-conn');
 var MongoStore = require('connect-mongo')(session);
 var constant = require('./mixin/constant');
 var yamlConfig = require('node-yaml-config');
 
+var captcha = require('./middleware/captcha');
 var config = yamlConfig.load(__dirname + '/config/config.yml');
 
 var env = ['production', 'test', 'staging', 'integration'].indexOf(process.env.NODE_ENV) < 0 ? 'development' : process.env.NODE_ENV;
+
 
 app.use(cookieParser());
 app.use(session({
@@ -28,9 +29,23 @@ app.use(session({
   })
 }));
 
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+var params = {
+  "url": "/captcha.jpg",
+  "color": "#ffffff",
+  "background": "#000000",
+  "lineWidth": 1,
+  "fontSize": 25,
+  "codeLength": 4,
+  "canvasWidth": 72,
+  "canvasHeight": 34
+};
+
+app.use(captcha(params));
 
 app.use(bodyParser.json());
 
@@ -38,15 +53,15 @@ app.use(sessionCheck);
 
 route.setRoutes(app);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send("Not Found!");
-})
+});
 
-app.use(function(err, req, res, next) {
-  if(err) {
+app.use(function (err, req, res, next) {
+  if (err) {
     res.status(500).send(err.stack);
   }
-})
+});
 
 app.listen(config.port, function () {
   console.log('Current environment is: ' + env);
