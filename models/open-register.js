d13f48ec911e8f74c619e160b9516056f546b167
purpletianjing <TianjingSun@outlook.com>
2016-04-21 11:17:29 +0800

    rename closeRegister to openRegister

---
+++ models/open-register.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var openRegisterSchema = new Schema({
+  isOpenRegister: Boolean
+});
+
+var openRegister = mongoose.model('openRegister', openRegisterSchema);
+openRegister.findOneAndUpdate({isOpenRegister: false},{isOpenRegister: false},
+  {upsert: true},function(err,numberAffected, raw){});
+
+module.exports = openRegister;
