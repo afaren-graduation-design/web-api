201182301b780a32dd7310410534944c912fa17d
wengjiaojiao <419909058@qq.com>
2016-04-21 15:37:45 +0800

    create admin routers

--- routes/routers/admin.js
+++ routes/routers/admin.js
@@ -1,3 +1,9 @@
-/**
- * Created by Jessica on 4/21/16.
- */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var adminController = require('../../controllers/admin-controller');
+
+router.get('/users-channel', adminController.getUsersChannel);
+
+module.exports = router;
