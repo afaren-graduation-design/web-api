655ddf9e2ac6297ce803f5447292549fd576c676
sialvsic <sialvsic@outlook.com>
2016-04-24 15:21:25 +0800

    extract login and register router to controller

---
+++ controllers/login-controller.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var express = require('express');
+var constant = require('../mixin/constant');
+var md5 = require('js-md5');
+var validate = require('validate.js');
+var constraint = require('../mixin/login-constraint');
+var apiRequest = require('../services/api-request');
+var async = require('async');
+
+
+function checkLoginInfo(account, password) {
+  var pass = true;
+  var valObj = {};
+
+  valObj.email = account;
+  valObj.mobilePhone = account;
+  valObj.loginPassword = password;
+  var result = validate(valObj, constraint);
+
+  if (!(result.email || result.mobilePhone)) {
+    pass = false;
+  }
+
+  if (password.length < constant.PASSWORD_MIN_LENGTH ||
+      password.length > constant.PASSWORD_MAX_LENGTH) {
+    pass = false;
+  }
+  return pass;
+}
+
+function LoginController() {
+
+}
+
+LoginController.prototype.login = (req, res, next) => {
+  var account = req.body.account;
+  var password = req.body.password;
+  var captcha = req.body.captcha;
+  var error = {};
+
+
+  async.waterfall([
+    (done)=> {
+      if (captcha !== req.session.captcha) {
+        error.status = constant.httpCode.FORBIDDEN;
+        done(error, null);
+      } else {
+        done(null, null);
+      }
+    }, (data, done)=> {
+      if (checkLoginInfo(account, password)) {
+        password = md5(password);
+        apiRequest.post('login', {email: account, password: password}, done);
+      } else {
+        error.status = constant.httpCode.UNAUTHORIZED;
+        done(error, null);
+      }
+    }, (result, done)=> {
+      if (result.body.id && result.headers) {
+        req.session.user = {
+          id: result.body.id,
+          role: result.body.role,
+          userInfo: result.body.userInfo
+        };
+        done(null, result);
+      } else {
+        error.status = constant.httpCode.UNAUTHORIZED;
+        done(error, null);
+      }
+    }], (error, result)=> {
+    if (error !== null && error.status === constant.httpCode.FORBIDDEN) {
+      res.send({status: constant.httpCode.FORBIDDEN});
+      return;
+    } else if (error !== null && error.status === constant.httpCode.UNAUTHORIZED) {
+      res.send({status: constant.httpCode.UNAUTHORIZED});
+      return;
+    } else if (result.status === constant.httpCode.OK) {
+      res.send({status: constant.httpCode.OK});
+      return;
+    }
+    return next(error);
+  });
+
+};
+
+module.exports = LoginController;
+
---
+++ controllers/register-controller.js
@@ -0,0 +1,201 @@
+'use strict';
+
+var express = require('express');
+var lang = require('..//mixin/lang-message/chinese');
+var constant = require('../mixin/constant').backConstant;
+var async = require('async');
+var validate = require('validate.js');
+var md5 = require('js-md5');
+var constraint = require('../mixin/register-constraint');
+var httpStatus = require('../mixin/constant').httpCode;
+var apiRequest = require('../services/api-request');
+var configuration = require('../models/configuration');
+var UserChannel = require('../models/user-channel');
+var mongoose = require('mongoose');
+
+function checkRegisterInfo(registerInfo) {
+  var pass = true;
+
+  var valObj = {};
+  valObj.email = registerInfo.email;
+  valObj.mobilePhone = registerInfo.mobilePhone;
+  valObj.password = registerInfo.password;
+
+  var result = validate(valObj, constraint);
+
+  if (result !== undefined) {
+    pass = false;
+  }
+
+  if (registerInfo.password.length < constant.PASSWORD_MIN_LENGTH ||
+      registerInfo.password.length > constant.PASSWORD_MAX_LENGTH) {
+    pass = false;
+  }
+  return pass;
+}
+
+function RegisterController() {
+
+}
+
+RegisterController.prototype.register = (req, res, next) => {
+  var registerInfo = req.body;
+  var error = {};
+
+  if (checkRegisterInfo(registerInfo)) {
+    var isMobilePhoneExist = false;
+    var isEmailExist = false;
+    var isCaptchaError = false;
+
+    async.waterfall([
+      (done)=> {
+        configuration.findOne({}, (err, data) => {
+          if (!data.registerable) {
+            error.status = httpStatus.UNAUTHORIZED;//401 未开放注册
+            done(error, data);
+          } else {
+            done(null, null);
+          }
+        });
+      },
+      (data, done)=> {
+        if (registerInfo.captcha !== req.session.captcha) {
+          error.status = httpStatus.FORBIDDEN;  //403 验证码错误
+          isCaptchaError = true;
+          done(error, null);
+        } else {
+          done(null, null);
+        }
+      },
+      (data, done)=> {
+        apiRequest.get('users', {field: 'mobilePhone', value: registerInfo.mobilePhone}, function (err, resp) {
+          if (resp.body.uri) {
+            isMobilePhoneExist = true;
+          }
+          done(err, resp);
+        });
+      },
+      (data, done) => {
+        apiRequest.get('users', {field: 'email', value: registerInfo.email}, function (err, resp) {
+          if (resp.body.uri) {
+            isEmailExist = true;
+          }
+          if (isMobilePhoneExist || isEmailExist) {
+            done(true, resp);
+          } else {
+            done(err, resp);
+          }
+        });
+      },
+      (data, done)=> {
+        delete registerInfo.captcha;
+        registerInfo.password = md5(registerInfo.password);
+        apiRequest.post('register', registerInfo, done);
+      },
+      (data, done) => {
+        if (req.cookies.channel !== '') {
+          var userChannel = new UserChannel({
+            userId: data.body.id,
+            channelId: new mongoose.Types.ObjectId(req.cookies.channel)
+          });
+          userChannel.save((err, doc) => {
+            done(err, doc);
+          });
+        } else {
+          done();
+        }
+      },
+      (data, done)=> {
+        apiRequest.post('login', {email: registerInfo.email, password: registerInfo.password}, done);
+      },
+      (data, done)=> {
+        if (data.body.id && data.headers) {
+          req.session.user = {
+            id: data.body.id,
+            role: data.body.role,
+            userInfo: data.body.userInfo
+          };
+        }
+        done(null, data);
+      }
+    ], (err, data) => {
+
+      if (err !== null && error.status === httpStatus.UNAUTHORIZED) {
+        res.send({
+          status: constant.FORBIDDEN,
+          registerable: data.registerable
+        });
+      } else if (err !== null && error.status === httpStatus.FORBIDDEN) {
+        res.send({
+          status: constant.FAILING_STATUS,
+          message: lang.EXIST,
+          data: {
+            isEmailExist: isEmailExist,
+            isMobilePhoneExist: isMobilePhoneExist,
+            isCaptchaError: isCaptchaError
+          }
+        });
+      } else if (err) {
+        return next(err);
+      } else {
+        res.send({
+          status: data.status
+        });
+      }
+    });
+  }
+};
+
+RegisterController.prototype.valdateMobilePhone = (req, res, next) => {
+  apiRequest.get('users', {field: 'mobilePhone', value: req.query.mobilePhone}, function (err, result) {
+    if (!result) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR);
+      res.send();
+      return;
+    }
+    if (result.body.uri) {
+      res.send({
+        status: constant.SUCCESSFUL_STATUS
+      });
+    } else {
+      res.send({
+        status: constant.FAILING_STATUS
+      });
+    }
+  });
+};
+
+RegisterController.prototype.valdateEmail = (req, res, next) => {
+  apiRequest.get('users', {field: 'email', value: req.query.email}, function (err, result) {
+    if (!result) {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR);
+      res.send();
+      return;
+    }
+    if (result.body.uri) {
+      res.send({
+        status: constant.SUCCESSFUL_STATUS
+      });
+    } else {
+      res.send({
+        status: constant.FAILING_STATUS
+      });
+    }
+  });
+};
+
+RegisterController.prototype.registerable = (req, res, next) => {
+  async.waterfall([
+    (done)=> {
+      configuration.findOne({}, done);
+    }], (err, data)=> {
+    if (err) return next(err);
+    res.send({
+      registerable: data.registerable,
+      status: constant.OK
+    })
+  })
+};
+
+
+module.exports = RegisterController;
