c6078748b8d7690bd75cc62143c2a4b98c9f2ddd
linwenjun <linwenjun@163.com>
2016-04-25 10:50:22 +0800

    add condition of paper-api

--- controllers/paper-controller.js
+++ controllers/paper-controller.js
@@ -3,7 +3,6 @@ var apiRequest = require('../services/api-request');
 function findOne(req, res, next) {
   apiRequest.get("papers/1", function(err, data) {
     if(err) {
-      console.log(err);
       return next(err);
     }
     res.send(data.body);
