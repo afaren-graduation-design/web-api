f8580349fb2622ce96ff10651dbf53bd2fd1e890
purpletianjing <TianjingSun@outlook.com>
2016-04-29 09:35:23 +0800

    modify spec

--- spec/integration-spec/dashboard-spec.js
+++ spec/integration-spec/dashboard-spec.js
@@ -3,15 +3,15 @@ var userSession = global.userSession;
 
 
 describe("GET /dashboard", ()=> {
-  it('should be return 200: GET /dashboard', function(done) {
+  it('should be return 200: GET /dashboard', function (done) {
     userSession
-    .get("/dashboard")
-    .expect(200)
+        .get("/dashboard")
+        .expect(200)
         .expect(function (res) {
-          res.body.isPaperCommited.should.equal(true);
+          res.body.isPaperCommited.should.equal(false);
           res.body.isOverTime.should.equal(false);
           res.body.isFinished.should.equal(false);
         })
-    .end(done);
+        .end(done);
   });
 });
