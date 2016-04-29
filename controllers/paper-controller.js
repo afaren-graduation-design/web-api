8c9d38ea851bdf41ba1e7db2e742d870ed9b659c
purpletianjing <TianjingSun@outlook.com>
2016-04-29 16:46:17 +0800

    /papers/:paperHash

--- controllers/paper-controller.js
+++ controllers/paper-controller.js
@@ -1,14 +1,82 @@
 var apiRequest = require('../services/api-request');
+var async = require('async');
+var UserPaper = require('../models/user-paper');
+var httpStatus = require('../mixin/constant').httpCode;
 
-function findOne(req, res, next) {
-  apiRequest.get("papers/1", function(err, data) {
-    if(err) {
-      return next(err);
+function details(req, res, next) {
+  var paperHash = req.params.paperHash;
+  var userId = req.session.user.id;
+  var paper = {};
+  var doc;
+  var paperIndex;
+  var error = {};
+
+  async.waterfall([
+    (done) => {
+      UserPaper.findOne({userId: userId}, done)
+    },
+
+    (data, done) => {
+      if (!data) {
+        done(true, null)
+      } else {
+        doc = data;
+        paper = data.papers.find((paper, index)=> {
+          paperIndex = index;
+          return paper._id == paperHash;
+        });
+        done(null, paper);
+      }
+    },
+
+    (data, done) => {
+      if (data.hasOwnProperty('sections') && data.sections.length !== 0) {
+        done(null, data);
+      } else {
+        apiRequest.get('papers/' + data.id, done);
+      }
+    },
+
+    (data, done) => {
+      if (paper.sections.length !== 0) {
+        done(null, paper);
+      } else {
+        var userPaper = new UserPaper({
+          userId: userId,
+          papers: []
+        });
+        doc.papers[paperIndex].sections = data.body.sections;
+
+        doc.save(function (err) {
+          if (err) {
+            error.status = httpStatus.INTERNAL_SERVER_ERROR;
+            done(error, null);
+          } else {
+            done(null, data);
+          }
+        });
+      }
+    },
+    (data, done) => {
+      done(null,null);
     }
-    res.send(data.body);
-  })
-}
 
-module.exports = {
-  findOne: findOne
+  ], (err, data) => {
+    if (err === true) {
+      res.send({
+        status: httpStatus.NOT_FOUND
+      });
+    } else if (err !== null && err.status === httpStatus) {
+      res.send({
+        status: httpStatus.INTERNAL_SERVER_ERROR
+      });
+    } else {
+      res.send({
+        status: httpStatus.OK
+      })
+    }
+  });
 }
+module.exports = {
+  details: details
+};
