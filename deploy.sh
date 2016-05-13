38a4bf577daced44cc617a40a10e5efa190923d3
linwenjun <linwenjun@163.com>
2016-05-13 10:45:27 +0800

    add restart for deploy

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,4 @@ zip -qr web-api.zip ./
 scp web-api.zip $REMOTE_SERVER_IP:/home/ubuntu/twars
 ssh -T $REMOTE_SERVER_IP 'unzip -qo /home/ubuntu/twars/web-api.zip -d /home/ubuntu/twars/web-api'
 ssh -T $REMOTE_SERVER_IP 'rm /home/ubuntu/twars/web-api.zip'
+docker restart assembly_web-api_1
