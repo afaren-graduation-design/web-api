41df8e2d83ebe322f4e619b1d58c85161c68374b
linwenjun <linwenjun@163.com>
2016-05-13 11:02:46 +0800

    fix issue of restart web-api

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ zip -qr web-api.zip ./
 scp web-api.zip $REMOTE_SERVER_IP:/home/ubuntu/twars
 ssh -T $REMOTE_SERVER_IP 'unzip -qo /home/ubuntu/twars/web-api.zip -d /home/ubuntu/twars/web-api || true'
 ssh -T $REMOTE_SERVER_IP 'rm /home/ubuntu/twars/web-api.zip'
-docker restart assembly_web-api_1
+ssh -T $REMOTE_SERVER_IP 'docker restart assembly_web-api_1'
