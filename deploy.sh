2d01c6de2a60a083de07e63004a50d204ccc7215
linwenjun <linwenjun@163.com>
2016-05-13 10:51:25 +0800

    convert unzip err to stdout

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
-PS4="command: "
+PS4=" > "
 set -x
+
 if [ -z $REMOTE_SERVER_IP ]
 then
   echo "Please Set the REMOTE_SERVER_IP"
@@ -10,9 +11,8 @@ then
 fi
 
 npm install
-
 zip -qr web-api.zip ./
 scp web-api.zip $REMOTE_SERVER_IP:/home/ubuntu/twars
-ssh -T $REMOTE_SERVER_IP 'unzip -qo /home/ubuntu/twars/web-api.zip -d /home/ubuntu/twars/web-api'
+ssh -T $REMOTE_SERVER_IP 'unzip -qo /home/ubuntu/twars/web-api.zip -d /home/ubuntu/twars/web-api 2>&1'
 ssh -T $REMOTE_SERVER_IP 'rm /home/ubuntu/twars/web-api.zip'
 docker restart assembly_web-api_1
