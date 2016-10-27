f1a7b218bc8f2df880ac81a55bc07a78e11b921b
banshengbushu <wangdanna1995@outlook.com>
2016-10-27 16:16:10 +0800

    fix conflict

--- routes/routers/papers.js
+++ routes/routers/papers.js
@@ -7,8 +7,5 @@ var PaperController = require('../../controllers/paper-controller');
 router.get('/:paperHash', PaperController.details);
 router.get('/:paperId/obtainment', PaperController.obtain);
 router.get('/',PaperController.getLists);
-<<<<<<< HEAD
 router.put('/:paperId',PaperController.deletePaper);
-=======
->>>>>>> 7916dbc420211190424ac18c35bb18cf3ea25c7b
 module.exports = router;
