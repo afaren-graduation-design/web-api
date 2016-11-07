795cfa43e391a35ce4e594d6793c79ce6c738a2b
wyb1995 <wy_b1995@163.com>
2016-11-07 18:29:52 +0800

    add checkSession function

--- routes/routers/check-cookie.js
+++ routes/routers/check-cookie.js
@@ -1,33 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
-const mongoConn = require('../../services/mongo-conn');
+var teacherSession = require('../../models/teacher-session');
 
-// mongoConn.start('mongodb://localhost/userCookie');
-
-const Schema = mongoose.Schema;
-
-const userCookie = new Schema({
-    userHash: String
-});
-
-const UserCookie = mongoose.model('userCookies', userCookie);
-
-router.get('/', (req, res) => {
-    const userHash = '12345678';
-
-    new UserCookie({userHash: '12345678'}).save((err)=> {
-        if (err)
-            return next(err);
-        UserCookie.findOne({userHash}, (err, user)=> {
-            if (err)
-                return next(err);
-            console.log(user);
-            if (user) {
-                return res.sendStatus(200);
-            }
-            return res.sendStatus(403);
-        })
-    })
+router.get('/', (req, res, next) => {
+  const session = req.cookies.teacher;
+  console.log(session);
+  teacherSession.findOne({userHash: session}, (err, user)=> {
+    if (err)
+      return next(err);
+    console.log(user);
+    if (user) {
+      return res.sendStatus(200);
+    }
+    return res.sendStatus(403);
+  })
 });
 module.exports = router;
