35dcc68baa7ea683ff5263deaeb06365ab8f80b9
Afaren <550047450@qq.com>
2016-11-11 11:01:09 +0800

    afaren - remove console.log

--- spec/support/mock-server.js
+++ spec/support/mock-server.js
@@ -5,15 +5,12 @@ var stdout;
 
 function startServer(config, done) {
   var checkCommand = "lsof -t -i:12306 | wc -l";
-  console.log(shelljs.exec(checkCommand));
   var result = shelljs.exec(checkCommand).stdout || '';
   isMockServerStarted = parseInt(result.trim()) > 0;
-  console.log("isStarted: " + isMockServerStarted);
   if(!isMockServerStarted) {
     console.log("Mock Server Was Stopped, Please Run: npm run mock-server".underline.red);
     done(new Error("Mock Server Was Stopped"));
   } else {
-	  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
     done();
   }
 }
