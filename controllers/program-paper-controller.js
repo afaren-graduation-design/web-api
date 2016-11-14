3aab90876c8b75f87aa37f7a242706f994917385
wyb1995 <wy_b1995@163.com>
2016-11-14 19:29:10 +0800

    add update_paper-api

--- controllers/program-paper-controller.js
+++ controllers/program-paper-controller.js
@@ -7,91 +7,43 @@ function ProgramPaperController() {
 }
 
 ProgramPaperController.prototype.getPaper = (req, res, next) => {
-    var programId = req.params.programId;
-    var paperId = req.params.paperId;
+  var programId = req.params.programId;
+  var paperId = req.params.paperId;
 
-    PaperDefinition.find({programId, _id: paperId}, (err, data) => {
-        if (!err && data) {
-            res.status(200).send(data);
-        } else {
-            next();
-        }
-    });
+  PaperDefinition.find({programId, _id: paperId}, (err, data) => {
+    if (!err && data) {
+      res.status(200).send(data);
+    } else {
+      next();
+    }
+  });
 };
 
 ProgramPaperController.prototype.savePaper = (req, res, next) => {
   var programId = req.params.programId;
   // var makerId = req.session.user.id;   //去看mocha获取用户身份的方式
-    var makerId = req.body.makerId
+  var makerId = req.body.makerId;
 
   var createTime = new Date().toDateString();
-  var {title,description,sections,makerId} = req.body;
+  var {title, description, sections, makerId} = req.body;
 
   new PaperDefinition({
-      programId,
-      isDistribution:false,
-      makerId,
-      description,
-      title,
-      createTime,
-      sections
-  }).save((err,paper)=>{
-      if( !err  && paper) {
-          res.status(201).send({
-              paperId:paper._id
-          });
-      } else {
-          res.sendStatus(constant.BAD_REQUEST);
-      }
+    programId,
+    isDistribution: false,
+    makerId,
+    description,
+    title,
+    createTime,
+    sections
+  }).save((err, paper)=> {
+    if (!err && paper) {
+      res.status(201).send({
+        paperId: paper._id
+      });
+    } else {
+      res.sendStatus(constant.BAD_REQUEST);
+    }
   });
-
-
-
-
-
-
-  // section.map(({title, description, type, quizzes}) => {
-  //   if (type === 'logicQuizzes') {
-  //     apiRequest.post('logicQuizzes', quizzes, (err, data) => {
-  //         if (err) {
-  //           return res.sendStatus(400);
-  //         }
-  //         logicSections.quizzes = data;
-  //       }
-  //     );
-  //     logicSections.title = title;
-  //     logicSections.description = description;
-  //     logicSections.type = type;
-  //   } else {
-  //     apiRequest.post('homeworkQuizzes', quizzes, (err, data) => {
-  //         if (err) {
-  //           return res.sendStatus(400);
-  //         }
-  //         homeworkSections.quizzes = data;
-  //       }
-  //     );
-  //     homeworkSections.title = title;
-  //     homeworkSections.description = description;
-  //     homeworkSections.type = type;
-  //   }
-  // });
-  //
-  // new ProgramPaper({
-  //   title,
-  //   description,
-  //   makerId,
-  //   programId,
-  //   createTime,
-  //   logicSections,
-  //   homeworkSections
-  // }).save(function (err, data) {
-  //   if (err) {
-  //     return res.sendStatus(400);
-  //   }
-  //   var paperId = data.paperId;
-  //   var uri = 'program/' + programId + '/paper/' + paperId;
-  //   res.status(201).send({uri});
-  // })
 };
 
 ProgramPaperController.prototype.updatePaper = (req, res, next) => {
@@ -101,20 +53,14 @@ ProgramPaperController.prototype.updatePaper = (req, res, next) => {
 
   var {title, description, sections, isDistribution} = req.body;
 
-  new PaperDefinition({
-    programId,
-    isDistribution,
-    title,
-    description,
-    updateTime,
-    sections
-  }).save((err, paper) => {
-    if(!err & paper){
-      res.sendStatus(200);
-    }else{
-      res.sendStatus(400);
-    }
-  })
+  PaperDefinition.update({_id: paperId},
+    {$set: {programId, updateTime, title, description, isDistribution, sections}}, (err, paper)=> {
+      if(!err && paper){
+        res.status(200).send({paperId: paper._id});
+      }else{
+        res.sendStatus(400);
+      }
+    })
 };
 
 module.exports = ProgramPaperController;
--- spec/integration-spec/program-paper-spec.js
+++ spec/integration-spec/program-paper-spec.js
@@ -1,47 +1,76 @@
 require('./spec-base');
 var userSession = global.userSession;
-describe('GET program/:programId/paper/:paperId',()=>{
-   it('should be return a paper: GET  program/:programId/paper/:paperId',(done)=>{
-       userSession
-           .get('/program/1/paper/1')
-           .expect(200)
-           .expect(function (res) {
-               res.body.id.should.equal(1);
-               res.body.programId.should.equal(1);
-               res.body.sections[0].description.should.equal("这是描述");
-               res.body.sections[0].SectionType.should.equal('blankQuizzes');
-               res.body.sections[0].quizzes.length.should.equal(2);
-           })
-           .end(done);
-   })
+describe('GET program/:programId/paper/:paperId', ()=> {
+  it('should be return a paper: GET  program/:programId/paper/:paperId', (done)=> {
+    userSession
+      .get('/program/1/paper/1')
+      .expect(200)
+      .expect(function (res) {
+        res.body.id.should.equal(1);
+        res.body.programId.should.equal(1);
+        res.body.sections[0].description.should.equal("这是描述");
+        res.body.sections[0].SectionType.should.equal('blankQuizzes');
+        res.body.sections[0].quizzes.length.should.equal(2);
+      })
+      .end(done);
+  })
 });
 
- describe('POST program/:id/papers', ()=>{
-     it.only('should be return a paperId',(done)=>{
-         userSession
-             .post('/program/1/papers')
-             .send({
-                 programId:1,
-                 isDistribution:false,
-                 description:'这是一个描述',
-                 title:'题目',
-                 sections:[
-                     {
-                         title:'logicQuizzes',
-                         quizzes:{
-                             easy:1,
-                             normal:1,
-                             hard:1
-                         }
-                     },{
-                        title:'homeworkQuizzes',
-                         quizzes:[
-                             {id:1,uri:'/homeworkQuizzes/1'},{id:2,uri:'/homeworkQuizzes/2'}
-                         ]
-                     }
-                 ]
-             })
-             .expect(201)
-             .end(done);
-     })
+describe('POST program/:id/papers', ()=> {
+  it('should be return a paperId', (done)=> {
+    userSession
+      .post('/program/1/papers')
+      .send({
+        programId: 1,
+        isDistribution: false,
+        description: '这是一个描述',
+        title: '题目',
+        sections: [
+          {
+            title: 'logicQuizzes',
+            quizzes: {
+              easy: 1,
+              normal: 1,
+              hard: 1
+            }
+          }, {
+            title: 'homeworkQuizzes',
+            quizzes: [
+              {id: 1, uri: '/homeworkQuizzes/1'}, {id: 2, uri: '/homeworkQuizzes/2'}
+            ]
+          }
+        ]
+      })
+      .expect(201)
+      .end(done);
+  })
+});
+
+describe('PUT program/:id/paper/:id', ()=> {
+  it.only('should be return a paperId', (done)=> {
+    userSession
+      .put('/program/1/paper/5829958a7007c23870a1d68a')
+      .send({
+        programId: 1,
+        paperId: 1,
+        title: "new title",
+        description: "update paper-api",
+        isDistribution: false,
+        sections: [
+          {
+            title: "logicQuizzes",
+            quizzes: {
+              easy: 1,
+              normal: 1,
+              hard: 1
+            }
+          }, {
+            title: "homeworkQuizzes",
+            quizzes: [{id: 1, uri: '/homeworkQuizzes/1'}, {id: 2, uri: '/homeworkQuizzes/2'}]
+          }
+        ]
+      })
+      .expect(200)
+      .end(done);
+  })
 });
