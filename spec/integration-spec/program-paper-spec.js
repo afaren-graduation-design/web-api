d0bfb8276806ce9dc943ad7f6d82f1ea4af7c7d2
wyb1995 <wy_b1995@163.com>
2016-11-16 11:00:07 +0800

    fix distribution api

--- spec/integration-spec/program-paper-spec.js
+++ spec/integration-spec/program-paper-spec.js
@@ -1,17 +1,74 @@
 require('./spec-base');
 var userSession = global.userSession;
-describe('GET program/:programId/paper/:paperId', ()=> {
-  it('should be return a paper: GET  program/:programId/paper/:paperId', (done)=> {
+
+describe('POST program/:id/papers', ()=> {
+  it('should be return a paperId', (done)=> {
     userSession
-      .get('/program/1/paper/1')
+      .post('/program/1/papers')
+      .send({
+        programId: 1,
+        isDistribution: false,
+        description: '这是一个描述',
+        title: '题目',
+        sections: [
+          {
+            title: 'logicQuizzes',
+            quizzes: {
+              easy: 1,
+              normal: 1,
+              hard: 1
+            }
+          }, {
+            title: 'homeworkQuizzes',
+            quizzes: [
+              {id: 1, uri: '/homeworkQuizzes/1'}, {id: 2, uri: '/homeworkQuizzes/2'}
+            ]
+          }
+        ]
+      })
+      .expect(201)
+      .end(done);
+  })
+});
+
+describe('GET program/:programId/paper/:paperId', ()=> {
+  xit('should be return a paper: GET  program/:programId/paper/:paperId', (done)=> {
+    userSession
+      .get('/program/1/paper/5829958a7007c23870a1d68a')
       .expect(200)
       .expect(function (res) {
-        res.body.id.should.equal(1);
-        res.body.programId.should.equal(1);
-        res.body.sections[0].description.should.equal("这是描述");
-        res.body.sections[0].SectionType.should.equal('blankQuizzes');
-        res.body.sections[0].quizzes.length.should.equal(2);
+        res.body.id.equal(1);
+        res.body.programId.equal(1);
+        res.body.sections[0].description.equal("这是描述");
+        res.body.sections[0].quizzes.length.equal(2);
       })
+      .end(done);
+  })
+});
+
+describe('PUT program/:id/paper/:id', ()=> {
+  it('should be return a paperId', (done)=> {
+    userSession
+      .put('/program/1/paper/5829958a7007c23870a1d68a')
+      .send({
+        title: "new title",
+        description: "update paper-api",
+        isDistribution: false,
+        sections: [
+          {
+            title: "logicQuizzes",
+            quizzes: {
+              easy: 1,
+              normal: 1,
+              hard: 1
+            }
+          }, {
+            title: "homeworkQuizzes",
+            quizzes: [{id: 1, uri: '/homeworkQuizzes/1'}, {id: 2, uri: '/homeworkQuizzes/2'}]
+          }
+        ]
+      })
+      .expect(204)
       .end(done);
   })
 });
@@ -46,83 +103,35 @@ describe('POST program/:id/papers', ()=> {
   })
 });
 
-describe('PUT program/:id/paper/:id', ()=> {
-  it('should be return a paperId', (done)=> {
+describe("GET program/:programId/papers", ()=> {
+  it('should be return paper list', (done)=> {
     userSession
-      .put('/program/1/paper/5829958a7007c23870a1d68a')
-      .send({
-        title: "new title",
-        description: "update paper-api",
-        isDistribution: false,
-        sections: [
-          {
-            title: "logicQuizzes",
-            quizzes: {
-              easy: 1,
-              normal: 1,
-              hard: 1
-            }
-          }, {
-            title: "homeworkQuizzes",
-            quizzes: [{id: 1, uri: '/homeworkQuizzes/1'}, {id: 2, uri: '/homeworkQuizzes/2'}]
-          }
-        ]
+      .get('/program/1/papers')
+      .query({
+        page: 1,
+        pageCount: 3
       })
       .expect(200)
+      .expect((res)=> {
+        res.body.length.should.equal(3);
+      })
+      .end(done)
+  })
+});
+describe('DELETE program/:id/paper/:id', ()=> {
+  it('should be return a httpCode', (done)=> {
+    userSession
+      .delete('/program/1/paper/5829958a7007c23870a1d68a')
+      .expect(204)
       .end(done);
   })
 });
 
- describe('POST program/:id/papers', ()=>{
-     it('should be return a paperId',(done)=>{
-         userSession
-             .post('/program/1/papers')
-             .send({
-                 programId:1,
-                 isDistribution:false,
-                 description:'这是一个描述',
-                 title:'题目',
-                 sections:[
-                     {
-                         title:'logicQuizzes',
-                         quizzes:{
-                             easy:1,
-                             normal:1,
-                             hard:1
-                         }
-                     },{
-                        title:'homeworkQuizzes',
-                         quizzes:[
-                             {id:1,uri:'/homeworkQuizzes/1'},{id:2,uri:'/homeworkQuizzes/2'}
-                         ]
-                     }
-                 ]
-             })
-             .expect(201)
-             .end(done);
-     })
-});
-
- describe("GET program/:programId/papers",()=>{
-     it.only('should be return paper list',(done)=>{
-         userSession
-             .get('/program/1/papers')
-             .query({
-                 page:1,
-                 pageCount:3
-             })
-             .expect(200)
-             .expect((res)=>{
-                res.body.length.should.equal(3);
-             })
-             .end(done)
-     })
- });
-describe('DELETE program/:id/paper/:id', ()=> {
-  it.only('should be return a httpCode', (done)=> {
+describe('PUT program/:id/paper/:id/distribution', ()=> {
+  it('should_be return_a httpCode', (done)=> {
     userSession
-      .delete('/program/1/paper/5829958a7007c23870a1d68a')
-      .expect(200)
+      .put('/program/1/paper/1/distribution')
+      .expect(204)
       .end(done);
   })
 });
