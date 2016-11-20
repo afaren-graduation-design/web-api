22ee43be48abf3871a82097c45743be564c66344
zhangpei <13488233237@163.com>
2016-11-20 14:16:31 +0800

    modify api bug

--- routes/routers/program-paper.js
+++ routes/routers/program-paper.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var ProgramPaperController = require('../../controllers/program-paper-controller');
 var programPaperController = new ProgramPaperController();
-
-
-router.post('/:programId/papers', programPaperController.savePaper);
 router.get('/:programId/papers',programPaperController.getPaperList);
+router.post('/:programId/papers', programPaperController.savePaper);
+
 router.get('/:programId/papers/selection',programPaperController.selectPaper);
 router.delete('/:programId/paper/:paperId', programPaperController.deletePaper);
 router.get('/:programId/paper/:paperId', programPaperController.getPaper);
