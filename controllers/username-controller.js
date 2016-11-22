73eec92581c27ad2cf3138b552bb5090a267db0d
zhangpei <13488233237@163.com>
2016-11-22 11:29:34 +0800

    add username api

---
+++ controllers/username-controller.js
@@ -0,0 +1,26 @@
+var apiRequest = require('../services/api-request');
+var Token = require('../models/token');
+
+function UsernameController () {
+
+}
+
+
+UsernameController.prototype.getUsername = (req, res) => {
+    var uuid = req.cookies.uuid;
+    Token.findOne({uuid}).exec((err,user)=>{
+        if(!err && user) {
+            apiRequest.get(`/users/${user.id}/detail`, (err, resp) => {
+                   if(!err && resp){
+                       res.status(200).send({username:resp.body.name})
+                   } else {
+                       res.sendStatus(404);
+                   }
+            })
+        }else {
+            next();
+        }
+    })
+};
+
+module.exports = UsernameController;
