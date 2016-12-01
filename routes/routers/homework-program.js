1ec5fdc0e83794493275ce731d7d2791a6ab13f5
wyb1995 <wy_b1995@163.com>
2016-12-01 10:53:59 +0800

    modify to eslint

--- routes/routers/homework-program.js
+++ routes/routers/homework-program.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var homeworkProgramController = require('../../controllers/homework-program-controller');
-var homeworkProgramController = new homeworkProgramController();
+var HomeworkProgramController = require('../../controllers/homework-program-controller');
+var homeworkProgramController = new HomeworkProgramController();
 
 router.get('/', homeworkProgramController.getHomeworkListByMysql);
 router.get('/selection', homeworkProgramController.matchHomeworkByMysql);
