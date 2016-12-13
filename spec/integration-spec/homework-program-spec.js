5d01a47575300b2cb65ca069e5204946307a63ed
wyb1995 <wy_b1995@163.com>
2016-12-13 20:36:30 +0800

    modify /hoemworks /homeworks/selection api

--- spec/integration-spec/homework-program-spec.js
+++ spec/integration-spec/homework-program-spec.js
@@ -7,13 +7,9 @@ describe("GET /homeworks/homeworkList", ()=> {
   it("should return mysql homeworkList", (done)=> {
     userSession
       .get('/homeworks')
-      .query({
-        page: 1,
-        pageCount: 3
-      })
       .expect(200)
       .expect((res) => {
-        res.body.homeworkList.length.should.equal(3)
+        res.body.homeworkList.length.should.equal(8)
       })
       .end(done)
   })
@@ -24,11 +20,9 @@ describe("GET /homeworks/homeworkList/selection", ()=> {
     userSession
       .get('/homeworks/selection')
       .query({
-        page: 1,
-        pageCount: 3,
         name: 'homework1'
       })
-      .expect(202)
+      .expect(200)
       .expect((res)=> {
         res.body.homeworkList.length.should.equal(1)
       })
