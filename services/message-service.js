ce6a37ac1baa74b4594584b528cbcef8652e274f
banshengbushu <wangdanna1995@outlook.com>
2016-12-22 21:02:20 +0800

    reslove conficts

--- services/message-service.js
+++ services/message-service.js
@@ -3,10 +3,9 @@ import Message from '../models/messages';
 
 export default class MessageService {
   operateMessage(data, callback) {
-
     async.waterfall([
-      (done)=> {
-        Message.update({'_id': data.messageId}, {state: 1}, done)
+      (done) => {
+        Message.update({'_id': data.messageId}, {state: 1}, done);
       }
     ], callback);
   }
