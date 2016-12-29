0687ad611a188b4464159c83beb8f84262680492
Aym-fuhong <18829287015@163.com>
2016-12-29 18:32:51 +0800

    modify error handle

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -2,22 +2,14 @@ import Message from '../models/messages';
 var async = require('async');
 import MessageService from '../services/message-service';
 var apiRequest = require('../services/api-request');
-var httpStatus = require('../mixin/constant').httpCode;
 
 export default class MessagesController {
 
-  search(req, res) {
+  search(req, res, next) {
     const from = req.session.user.id;
     async.waterfall([
       (done) => {
         Message.find({from: from}, done);
-      }, (data, done) => {
-        if (!data) {
-          var error = httpStatus.NOT_FOUND;
-          done(error, null);
-        } else {
-          done(null, data);
-        }
       },
       (data, done) => {
         var mentorOfStudent = data.map(message => {
@@ -33,35 +25,22 @@ export default class MessagesController {
             }
             callback(null, Object.assign({}, {name: res.body.name}, {state: mentor.state}));
           });
-        }, (err, result) => {
-          done(err, result);
-        });
+        }, done);
       }],
-    (err, result, next) => {
-      if (result) {
-        res.send(result);
-      } else if (err === httpStatus.NOT_FOUND) {
-        res.send({
-          status: httpStatus.NOT_FOUND
+        (err, data) => {
+          if (err) {
+            return next(err);
+          } else {
+            res.status(200).send(data);
+          }
         });
-      } else {
-        next(err);
-      }
-    });
   }
 
-  findUnread(req, res) {
+  findUnread(req, res, next) {
     const to = req.session.user.id;
     async.waterfall([
       (done) => {
         Message.find({to: to, state: 0}, done);
-      }, (data, done) => {
-        if (!data) {
-          var error = httpStatus.NOT_FOUND;
-          done(error, null);
-        } else {
-          done(null, data);
-        }
       },
       (data, done) => {
         async.map(data, (message, callback) => {
@@ -70,25 +49,18 @@ export default class MessagesController {
               callback(err, null);
             }
             callback(null, Object.assign({},
-              {
-                _id: message._id,
-                from: message.from,
-                to: message.to,
-                type: message.type,
-                deeplink: message.deeplink,
-                state: message.state
-              },
-              {name: res.body.name}));
+                    {_id: message._id, from: message.from, to: message.to, type: message.type, deeplink: message.deeplink, state: message.state},
+                    {name: res.body.name}));
           });
         }, done);
       }],
-      (err, results) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.status(200).send(results);
-        }
-      });
+        (err, data) => {
+          if (err) {
+            return next(err);
+          } else {
+            res.status(200).send(data);
+          }
+        });
   }
 
   create(req, res) {
