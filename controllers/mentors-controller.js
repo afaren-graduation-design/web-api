452fff9e7d4536f42bdcb8e419eb550114937304
bmeritter <bme_ritter@foxmail.com>
2017-01-04 13:33:29 +0800

    fix bug

--- controllers/mentors-controller.js
+++ controllers/mentors-controller.js
@@ -15,26 +15,10 @@ export default class MentorsController {
 
   findMentorOfStudent(req, res, next) {
     const to = req.session.user.id;
+    let mentors = [];
     async.waterfall([
       (done) => {
         Message.find({to: to}, done);
-      },
-      (data, done) => {
-        Message.find({from: to}, (err, doc) => {
-          done(err, data.concat(doc));
-        });
-      },
-      (data, done) => {
-        let msgObj = [];
-        data.forEach((item) => {
-          let exit = msgObj.find((msg) => {
-            return item._id.equals(msg._id);
-          });
-          if (!exit) {
-            msgObj.push(item);
-          }
-        });
-        done(null, msgObj);
       },
       (data, done) => {
         var mentorOfStudent = data.filter(message => {
@@ -43,14 +27,49 @@ export default class MentorsController {
         done(null, mentorOfStudent);
       },
       (data, done) => {
-        async.map(data, (mentor, callback) => {
-          apiRequest.get(`users/${mentor.to}/detail`, (err, res) => {
+        async.map(data, (item, callback) => {
+          apiRequest.get(`users/${item.from}/detail`, (err, res) => {
             if (err) {
               callback(err, null);
+            } else {
+              callback(null, Object.assign({}, {name: res.body.name, type: item.type, '_id': item._id}));
             }
-            callback(null, Object.assign({}, {name: res.body.name}, {state: mentor.state}, {type: mentor.type}));
           });
         }, done);
+      },
+      (data, done) => {
+        mentors = data;
+        Message.find({from: to}, done);
+      },
+      (data, done) => {
+        var mentorOfStudent = data.filter(message => {
+          return message.type.indexOf('INVITATION') !== -1;
+        });
+        done(null, mentorOfStudent);
+      },
+      (data, done) => {
+        async.map(data, (item, callback) => {
+          apiRequest.get(`users/${item.to}/detail`, (err, res) => {
+            if (err) {
+              callback(err, null);
+            } else {
+              callback(null, Object.assign({}, {name: res.body.name, type: item.type, '_id': item._id}));
+            }
+          });
+        }, done);
+      },
+      (data, done) => {
+        mentors = mentors.concat(data);
+        let msgObj = [];
+        mentors.forEach((item) => {
+          let exit = msgObj.find((msg) => {
+            return item._id.equals(msg._id);
+          });
+          if (!exit) {
+            msgObj.push(item);
+          }
+        });
+        done(null, msgObj);
       }],
     (err, data) => {
       if (err) {
