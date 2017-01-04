74cb797c46769460d3ea641ea0bad0325d5bf8e1
zhangpei <13488233237@163.com>
2017-01-04 15:32:50 +0800

    modify test

--- spec/unit-test/services/paper-service-spec.js
+++ spec/unit-test/services/paper-service-spec.js
@@ -20,7 +20,7 @@ describe('paperSrv', () => {
     })
   });
 
-  it.only('getSection() should return sections status object', function (done) {
+  it('getSection() should return sections status object', function (done) {
     paperSrv.getSection({userId: 1, programId: 1, paperId: 1}, (err, data) => {
       done();
     })
