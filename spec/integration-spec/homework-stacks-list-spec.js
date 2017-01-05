517f47adfcc28ca427a4032490b14ff1f0270aff
rangel <18792561236@163.com>
2017-01-05 16:54:04 +0800

    Modify get /stack data

--- spec/integration-spec/homework-stacks-list-spec.js
+++ spec/integration-spec/homework-stacks-list-spec.js
@@ -5,13 +5,12 @@ require('should');
 describe("GET /stacks", ()=> {
   it('should be return 200: GET /stacks', function (done) {
     userSession
-      .get("/stacks")
-      .expect(200)
-      .expect(function (res) {
-        console.log(res.body.items[0].stackId)
-        res.body.items[0].stackId.should.equal(2);
-        res.body.items[0].title.should.equal('C#');
-      })
-      .end(done)
+        .get("/stacks")
+        .expect(200)
+        .expect(function (res) {
+          res.body.items[0].stackId.should.equal(1);
+          res.body.items[0].title.should.equal('Java');
+        })
+        .end(done)
   });
 });
