03582616d766534d837a5e5aef8de069f7b9ce04
Yvette14 <libra1014tyb@163.com>
2017-01-05 15:56:32 +0800

    fix bug

--- spec/unit-test/services/message-service-spec.js
+++ spec/unit-test/services/message-service-spec.js
@@ -68,7 +68,7 @@ describe('ToggleToReadHandler', () => {
     const ToggleToRead = new ToggleToReadHandler();
     ToggleToRead.handle(msgObj, (err, data) => {
       Message.findById('585bc4e613c65e2f61fede25', (err, doc) => {
-        const {state} = doc.toJson();
+        const {state} = doc.toJSON();
         state.should.equal(1);
         done(err);
       });
