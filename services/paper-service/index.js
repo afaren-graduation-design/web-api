8bbe15fd5d103be25ac5d69b89461db2ad1f9571
chengyurou <18829292695@163.com>
2017-01-05 15:39:15 +0800

    paper-service child rename

--- services/paper-service/index.js
+++ services/paper-service/index.js
@@ -3,8 +3,8 @@ import async from 'async';
 import Paper from '../../models/paper';
 import LogicPuzzleHandler from './paper-logic-puzzle-handler';
 import HomeworkQuizHandler from './paper-homework-quiz-handler';
-import LogicPuzzleSectionService from './logic-puzzle-section-service';
-import HomeWorkQuizSectionService from './homework-quiz-section-service';
+import LogicPuzzleSectionService from './logic-puzzle-section';
+import HomeWorkQuizSectionService from './homework-quiz-section';
 const handlerMap = {
   'blankQuizzes': new LogicPuzzleHandler(),
   'homeworkQuizzes': new HomeworkQuizHandler()
