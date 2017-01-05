a46020b131e71a837a54f5088484c3d47b863927
wyb1995 <wy_b1995@163.com>
2017-01-05 15:27:16 +0800

    delete constructor

--- controllers/program-paper-controller.js
+++ controllers/program-paper-controller.js
@@ -2,14 +2,11 @@ import apiRequest from '../services/api-request';
 import PaperService from '../services/paper-service/index';
 const paperService = new PaperService();
 class ProgramPaperController {
-  // constructor() {
-  //   this.paperService = new PaperService();
-  // };
 
   getSection(req, res, next) {
-    var programId = req.params.programId;
-    var paperId = req.params.paperId;
-    var userId = req.session.user.id;
+    let programId = req.params.programId;
+    let paperId = req.params.paperId;
+    let userId = req.session.user.id;
 
     paperService.getSection({programId, _id: paperId, userId}, (err, data) => {
       if (err) {
@@ -20,7 +17,7 @@ class ProgramPaperController {
   }
 
   getPaperList(req, res) {
-    var programId = req.params.programId;
+    let programId = req.params.programId;
     apiRequest.get(`programs/${programId}/papers`, (err, resp) => {
       if (err) {
         return res.sendStatus(400);
@@ -32,9 +29,9 @@ class ProgramPaperController {
   };
 
   retrievePaper(req, res, next) {
-    var programId = req.params.programId;
-    var paperId = req.params.paperId;
-    var userId = req.session.user.id;
+    let programId = req.params.programId;
+    let paperId = req.params.paperId;
+    let userId = req.session.user.id;
     paperService.retrieve({programId, paperId, userId}, (err, data) => {
       if (err) {
         return next(err);
