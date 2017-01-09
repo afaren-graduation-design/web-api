512d091f90eba451402a4a86b3e26cabcb4c5123
bmeritter <bme_ritter@foxmail.com>
2017-01-09 14:58:11 +0800

    use return replace else

--- services/mentor-service/MentorService.js
+++ services/mentor-service/MentorService.js
@@ -13,41 +13,42 @@ export default class MentorService {
       },
       (data, done) => {
         if (!data.length) {
-          done(null, data);
-        } else {
-          apiRequest.get(`users/${data.toString()}/detail`, (err, res) => {
-            const userList = res.body.userList ? res.body.userList : [].concat(res.body);
-            done(err, userList);
-          });
+          return done(null, data);
         }
+        apiRequest.get(`users/${data.toString()}/detail`, (err, res) => {
+          const userList = res.body.userList ? res.body.userList : [].concat(res.body);
+          done(err, userList);
+        });
       },
       (data, done) => {
         mentors = data.map(({userId, name}) => {
           return {userId, name, type: 'AGREE_INVITATION'};
         });
-        Message.find({from: to, type: 'INVITATION'}, done);
+        Message.find({from: to, type: 'INVITATION', state: 0}, done);
       },
       (data, done) => {
         if (!data.length) {
-          done(null, null);
-        } else {
-          let mentorIds = [];
-          data.forEach((item) => {
-            let exit = mentorIds.find(id => {
-              return id === item.to;
-            });
-            if (!exit) {
-              mentorIds.push(item.to);
-            }
-          });
-          apiRequest.get(`users/${mentorIds.toString()}/detail`, (err, res) => {
-            const userList = res.body.userList ? res.body.userList : [].concat(res.body);
-            let mentorObj = userList.map(({userId, name}) => {
-              return {userId, name, type: 'INVITATION'};
-            });
-            done(err, mentorObj);
-          });
+          return done(null, null);
         }
+        let mentorIds = [];
+        data.forEach((item) => {
+          let exit = mentorIds.find(id => {
+            return id === item.to;
+          });
+          if (!exit) {
+            mentorIds.push(item.to);
+          }
+        });
+        done(null, mentorIds);
+      },
+      (data, done) => {
+        apiRequest.get(`users/${data.toString()}/detail`, (err, res) => {
+          const userList = res.body.userList ? res.body.userList : [].concat(res.body);
+          let mentorObj = userList.map(({userId, name}) => {
+            return {userId, name, type: 'INVITATION'};
+          });
+          done(err, mentorObj);
+        });
       }], (err, data) => {
       if (data) {
         mentors = mentors.concat(data);
