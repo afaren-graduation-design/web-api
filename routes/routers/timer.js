9c1647c94a211875298ccfa73a607a435f0fc465
chengyurou <18829292695@163.com>
2017-01-10 15:31:14 +0800

    /api/timer/remaintime

--- routes/routers/timer.js
+++ routes/routers/timer.js
@@ -4,15 +4,12 @@ var express = require('express');
 var router = express.Router();
 var constant = require('../../mixin/constant');
 var _timeBase = 90;
-var logicPuzzle = require('../../models/logic-puzzle');
+var Paper = require('../../models/paper');
 var async = require('async');
-
-router.get('/remain-time', function(req, res) {
+router.get('/remain-time', (req, res) => {
   var TOTAL_TIME = _timeBase * constant.time.SECONDS_PER_MINUTE;
   var userId = req.session.user ? req.session.user.id : 'invalid';
-  var sectionId = req.query.sectionId ? parseInt(req.query.sectionId) : 1;
-  var programId = parseInt(req.query.programId);
-  var paperId = parseInt(req.query.paperId);
+  var sectionId = req.query.sectionId;
   var startTime;
   var thisSection;
 
@@ -21,48 +18,30 @@ router.get('/remain-time', function(req, res) {
       if (userId === 'invalid') {
         done(true);
       } else {
-        logicPuzzle.findOne({userId, programId, paperId}, done);
-      }
-    },
-    (logicPuzzle, done) => {
-      if (logicPuzzle && logicPuzzle.sections.length === 0) {
-        startTime = Date.parse(new Date()) / constant.time.MILLISECOND_PER_SECONDS;
-
-        logicPuzzle.sections.push({
-          startTime: startTime,
-          sectionId: sectionId
-        });
-
-        logicPuzzle.save((err, doc) => {
-          done(err, doc);
-        });
-      } else if (logicPuzzle && logicPuzzle.sections.length !== 0) {
-        thisSection = logicPuzzle.sections.find((section) => {
-          return section.sectionId === sectionId;
-        });
-
-        if (thisSection) {
-          done(null, logicPuzzle);
-        } else {
+        Paper.findOne({'sections._id': sectionId}).exec((err, doc) => {
+          if (err) {
+            done(err, null);
+          }
           startTime = Date.parse(new Date()) / constant.time.MILLISECOND_PER_SECONDS;
-
-          logicPuzzle.sections.push({
-            startTime: startTime,
-            sectionId: sectionId
-          });
-
-          logicPuzzle.save((err, doc) => {
-            done(err, doc);
-          });
-        }
-      } else {
-        done(null, logicPuzzle);
+          thisSection = doc.sections.find(section => section._id + '' === sectionId);
+          if (thisSection.hasOwnProperty(startTime)) {
+            done(null, doc);
+          } else {
+            var sectionIndex = doc.sections.indexOf(thisSection);
+            doc.sections[sectionIndex].startTime = startTime;
+            doc.save((err, doc) => {
+              if (err) {
+                done(err, null);
+              }
+              done(null, doc);
+            });
+          }
+        });
       }
     },
-    (logicPuzzle, done) => {
+    (doc, done) => {
       var now = Date.parse(new Date()) / constant.time.MILLISECOND_PER_SECONDS;
       var usedTime = now - (startTime || thisSection.startTime);
-
       done(null, parseInt((TOTAL_TIME - usedTime)));
     }
   ], (err, remainTime) => {
