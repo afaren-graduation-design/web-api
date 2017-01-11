8cdffe78f97ec61b18236269d8dfd87c148cc835
Yvette14 <libra1014tyb@163.com>
2017-01-11 11:05:57 +0800

    modify images path

--- services/question-service/LogicPuzzleHandler.js
+++ services/question-service/LogicPuzzleHandler.js
@@ -1,4 +1,6 @@
 import async from 'async';
+import yamlConfig from 'node-yaml-config';
+const config = yamlConfig.load('./config/config.yml');
 import OperateHandler from './OperateHandler';
 import Paper from '../../models/paper';
 
@@ -11,20 +13,20 @@ export default class LogicPuzzleHandler extends OperateHandler {
     async.waterfall([
       (done) => {
         Paper.aggregate()
-            .unwind('$sections')
-            .unwind('$sections.quizzes')
-            .exec((err, doc) => {
+          .unwind('$sections')
+          .unwind('$sections.quizzes')
+          .exec((err, doc) => {
+            if (err) {
+              return done(err, null);
+            }
+            Paper.populate(doc, {path: 'sections.quizzes.quizId'}, (err, docs) => {
               if (err) {
                 return done(err, null);
               }
-              Paper.populate(doc, {path: 'sections.quizzes.quizId'}, (err, docs) => {
-                if (err) {
-                  return done(err, null);
-                }
-                const itemCount = docs.filter((item) => item.sections.quizzes.quizId.__t === 'LogicPuzzle').length;
-                done(null, itemCount);
-              });
+              const itemCount = docs.filter((item) => item.sections.quizzes.quizId.__t === 'LogicPuzzle').length;
+              done(null, itemCount);
             });
+          });
       },
       (data, done) => {
         const logicPuzzle = {
@@ -33,7 +35,7 @@ export default class LogicPuzzleHandler extends OperateHandler {
             initializedBox: JSON.parse(quiz.initializedBox),
             question: quiz.question,
             description: JSON.parse(quiz.description),
-            chartPath: quiz.chartPath
+            chartPath: config.staticFileServer + quiz.chartPath
           },
           itemCount: data
         };
