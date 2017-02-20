e2f526dd6561487d8e1299fb0b35c54ced571401
Yvette14 <libra1014tyb@163.com>
2017-02-20 20:50:23 +0800

    modify stack controller

--- controllers/stakcs-controller.js
+++ controllers/stakcs-controller.js
@@ -1,34 +1,39 @@
 'use strict';
+var request = require('superagent');
 var apiRequest = require('../services/api-request');
+var constant = require('../mixin/constant');
 
 function StacksController() {
 };
 
-StacksController.prototype.getStacks = (req, res) => {
+StacksController.prototype.getStacks = (req, res, next) => {
   apiRequest.get('stacks', (err, resp) => {
     if (err) {
-      return res.sendStatus(400);
+      return next(err);
     }
-    return res.send(resp.body);
+    return res.status(constant.httpCode.OK).send(resp.body);
   });
 };
 
-StacksController.prototype.getStack = (req, res) => {
+StacksController.prototype.getStack = (req, res, next) => {
   apiRequest.get(`stacks/${req.params.stackId}`, (err, resp) => {
     if (err) {
-      return res.sendStatus(404);
+      return next(err);
     }
-    return res.send(resp.body);
+    if (resp.statusCode === 404) {
+      return res.sendStatus(constant.httpCode.NOT_FOUND);
+    }
+    return res.status(constant.httpCode.OK).send(resp.body);
   })
 };
 
-StacksController.prototype.create = (req, res) => {
+StacksController.prototype.create = (req, res, next) => {
   const stack = req.body;
   apiRequest.post('stacks', stack, (err, resp) => {
     if (err) {
-      return res.sendStatus(400);
+      return next(err);
     }
-    return res.send(resp.body);
+    return res.status(constant.httpCode.CREATED).send(resp.body);
   })
 };
 
