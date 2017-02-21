307f9a96bcb76cce18f86151b031a270c19d8b3c
bmeritter <bme_ritter@foxmail.com>
2017-02-21 17:21:17 +0800

    check definition

--- controllers/stakcs-controller.js
+++ controllers/stakcs-controller.js
@@ -29,12 +29,30 @@ StacksController.prototype.getStack = (req, res, next) => {
 
 StacksController.prototype.create = (req, res, next) => {
   const stack = req.body;
-  apiRequest.post('stacks', stack, (err, resp) => {
-    if (err) {
-      return next(err);
-    }
-    return res.status(constant.httpCode.CREATED).send(resp.body);
-  })
+  const definitions = stack.definition.split(' ');
+  request
+    .get(`https://registry.hub.docker.com/v1/repositories/${definitions[0]}/tags`)
+    .end((err, response) => {
+      if (response.statusCode !== constant.httpCode.OK) {
+        return res.sendStatus(constant.httpCode.BAD_REQUEST);
+      }
+
+      const tags = response.body;
+      const exit = tags.find((tag) => {
+        return tag.name === definitions[1]
+      });
+      if (definitions.length !== 1 && !exit) {
+        return res.sendStatus(constant.httpCode.BAD_REQUEST);
+      }
+
+      apiRequest.post('stacks', stack, (err, resp) => {
+        if (err) {
+          return next(err);
+        }
+        return res.status(constant.httpCode.CREATED).send(resp.body);
+      })
+    });
+
 };
 
 module.exports = StacksController;
