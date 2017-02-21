1979b1094c4b1c5024decb773a483184c17320fe
bmeritter <bme_ritter@foxmail.com>
2017-02-21 17:57:34 +0800

    rename stakcs-controller to stacks-controller

--- routes/routers/stacks.js
+++ routes/routers/stacks.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var StacksController = require('../../controllers/stakcs-controller');
+var StacksController = require('../../controllers/stack-controller');
 var stacksController = new StacksController();
 
 router.get('/', stacksController.getStacks);
