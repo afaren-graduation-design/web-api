cff23b41c7feb7c3891d3d5454dececee35e3393
Miss-qi <1370322806@qq.com>
2017-02-24 10:55:02 +0800

    add status for stack

--- models/stack.js
+++ models/stack.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const stackSchema = new Schema({
   title: String,
   description: String,
-  definition: String
+  definition: String,
+  status: Number
 });
 
 const Stack = mongoose.model('Stack', stackSchema);
